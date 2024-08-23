326940fbb7f82f082a6734b6cdac0837cd1c13b8
Thousif Baba <135221733+Thousif-Baba@users.noreply.github.com>
2024-08-23 18:50:56 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
+    content: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Post', postSchema);
